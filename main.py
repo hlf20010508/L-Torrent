5f70479e3c974337e22c304c411ceb66770d04dc
L-ING <hlf01@icloud.com>
2023-11-30 12:08:12 +0800

    main.Client重新手动使用start

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ class Client(object):
 
         self.peers_scraper.start()
         self.peers_manager.start()
-        self.start()
         # print("PeersManager Started")
         # print("PiecesManager Started")
         # self.tracker.find_peers()
@@ -105,6 +104,6 @@ if __name__ == '__main__':
     magnet_link = "magnet:?xt=urn:btih:dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c&dn=Big+Buck+Bunny&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fbig-buck-bunny.torrent"
     port = 8080
     timeout = 2
-    # run = Client(magnet_link, port, timeout)
-    Client(magnet_link, port, timeout)
-    # run.start()
+
+    client = Client(magnet_link, port, timeout)
+    client.start()
